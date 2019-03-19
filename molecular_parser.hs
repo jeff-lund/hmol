922cb1384ce3457bfa3873ac35191a1165388225
jeff-lund <jdl3@pdx.edu>
2019-03-19 13:58:47 -0700

    working on smilesifying graphs:

--- molecular_parser.hs
+++ molecular_parser.hs
@@ -2,37 +2,14 @@ import Data.Graph
 import Data.List
 import Data.Array
 
-data Atom = H  | He | Li | Be | B  | C  | N  | O  | F  | Ne | Na | Mg | Al |
-            Si | P  | S  | Cl | Ar | K  | Ca | Sc | Ti | V  | Cr | Mn | Fe |
-            Co | Ni | Cu | Zn | Ga | Ge | As | Se | Br | Kr | Rb | Sr | Y  |
-            Zr | Nb | Mo | Tc | Ru | Rh | Pd | Ag | Cd | In | Sn | Sb | Te |
-            I  | Xe | Cs | Ba | Hf | Ta | W  | Re | Os | Ir | Pt | Au | Hg |
-            Tl | Pb | Bi | Po | At | Rn | Fr | Ra | Rf | Db | Sg | Bh | Hs |
-            Mt | Ds | Rg | Cn | Fl | Lv | La | Ce | Pr | Nd | Pm | Sm | Eu |
-            Gd | Tb | Dy | Ho | Er | Tm | Yb | Lu | Ac | Th | Pa | U  | Np |
-            Pu | Am | Cm | Bk | Cf | Es | Fm | Md | No | Lr | Star |
-            B_| C_ | N_ | O_ | P_ | S_ | Se_ | As_ -- armoatics
-            deriving (Show, Eq)
 
-data Bond = Single | Double | Triple | Aromatic
-
-organics = [B, C, N, O, S, P, F, Cl, Br, I, B_, C_, N_, O_, S_, P_, Star]
-
-bondOrder     :: Atom -> Maybe [Int]
-bondOrder a | a == C                = Just [4]
-            | a == N                = Just [3, 5]
-            | a == O                = Just [2]
-            | a == P                = Just [3, 5]
-            | a == S                = Just [2, 4, 6]
-            | elem a [F, Br, Cl, I] = Just [1]
-            | a == Star             = Just []
-            | otherwise             = Nothing
-
+bondDict n | n > 1 = "+#$" !! (n - 1)
+           | otherwise = ""
 --aceticAcid :: ([[Int]], [Atom])
 aceticAcid = ([[0, 1, 2, 1],
                [1, 0, 0, 0],
                [2, 0, 0, 0],
-               [1, 0, 0, 0]], [C, C, O, O])
+               [1, 0, 0, 0]], "CCOO")
 
 aceticAcidSMILES = "CC(O)=O"
 
@@ -42,33 +19,32 @@ cyclicMol = ([[0, 1, 0, 0, 1, 0],
               [0, 1, 0, 1, 0, 0],
               [0, 0, 1, 0, 1, 0],
               [1, 0, 0, 1, 0, 1],
-              [0, 0, 0, 0, 1, 0]] , [C, C, C, C, C, O])
+              [0, 0, 0, 0, 1, 0]] , "CCCNCO")
 
 cmEL = adjToEdges (fst cyclicMol)
 aaEL = adjToEdges (fst aceticAcid)
 cmG  = toGraph cmEL
 aaG  = toGraph aaEL
 
+simpleEdgeList [] = []
+simpleEdgeList (e:es) = if elem e es then simpleEdgeList es else e: simpleEdgeList es
+
+edgeListToAdjList [] = []
+edgeListToAdjList el = zip [0..m] [[snd e | e <- el, fst e == i] | i <- [0..m]]
+    where m = maximum $ concat [[fst x, snd x] | x <- el]
+
 adjToEdges []     = []
 adjToEdges rs = convert 0 0 rs
-
-adjToSimpleEdges [] = []
-adjToSimpleEdges m  = convert' 0 m
 
 convert n k [] = []
 convert n k (r:rs) = concat [multiEdge c (n, ind) | (c, ind) <- zip r [0..], c > k, ind > n]
                      ++ convert (n+1) k rs
 
-
-convert' n [] = []
-convert' n (r:rs) = [(n, ind) | (c, ind) <- zip r [0..], c > 0, ind > n]
-                     ++ convert' (n+1) rs
-
 multiEdge 0 e = []
 multiEdge n e =  e:(multiEdge (n-1) e)
 
-toGraph edgeList = buildG (0, m) edgeList
-              where m = maximum $ concat [[fst x, snd x] | x <- edgeList]
+toGraph el = buildG (0, m) el
+              where m = maximum $ nub (map fst el ++ map snd el)
 
 -- build up a minmum spanning tree
 prims :: [(Int, Int)] -> [(Int, Int)]
@@ -85,3 +61,35 @@ findEdge ns [] = error "No edges found"
 findEdge ns (e:es) | (elem (fst e) ns) && (notElem (snd e) ns) = ((snd e), e)
                    | (notElem (fst e) ns) && (elem (snd e) ns) = ((fst e), e)
                    | otherwise = findEdge ns es
+
+cyclicEdges el = [e | e <- el, notElem e p]
+      where p = prims el
+
+bondCount el = [countBond e el | e <- p]
+      where p = prims el
+
+countBond e el = (c, e)
+      where c = length [e' | e' <- el, e' == e]
+
+contents i []     = []
+contents i (a:as) | i == fst a    = snd a
+                  | otherwise = contents i as
+
+smilesify mol n = key !! n ++ ringChk n cyc bc ++ smileMore (contents n al)
+      where key = snd mol
+            el = adjToEdges (fst mol)
+            al = edgeListToAdjList el
+            bc  = bondCount el
+            cyc = cyclicEdges el
+            m = length key
+
+bondChk         :: (Int, Int) -> [(Int, Int)] -> [Char]
+bondChk e []     = ""
+bondChk e (b:bs) | e == snd b = bondDict fst b
+                 | ringBnd e bs
+
+ringChk                     :: Int -> [(Int, Int)] -> [Char]
+ringChk n [] _               = ""
+ringChk n cyc bc | elem n v  = concat [show i ++ bondChk e bc | (i, e) <- zip [0..] cyc, n == fst e || n == snd e]
+                 | otherwise = ""
+                 where v = nub (map fst cyc ++ map snd cyc)
